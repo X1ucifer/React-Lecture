d1cc12d4e012a23870a08231b580209297998400
Akhil Jinu <portforwarding03@gmail.com>
2022-02-16 21:39:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from "axios"
+import Demo from './demo';
 
 function App() {
+
+  const [product, setProduct] = useState([]);
+
+  const getData = async()=>{
+    try{
+      const data = await axios.get("http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline");
+      console.log(data.data)
+      setProduct(data.data)
+
+    }catch(err){
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, [])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <h1 style={{textAlign:"center"}}>Let's code</h1>
+     {product.map((item) => {
+       return(
+         <p>
+           {item.price}
+         </p>
+       )
+     })}
+    </>
   );
+
 }
 
 export default App;
---
+++ src/demo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./App.css"
+
+function Demo({data}) {
+    
+    return (
+        <div>
+            <h1>{data}</h1>
+        </div>
+    )
+}
+
+export default Demo
